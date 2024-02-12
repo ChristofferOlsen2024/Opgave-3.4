85585875dab60a145eb7548b56200d172e8a7932
Christoffer Olsen <chris70944@gmail.com>
2024-02-12 14:30:32 +0100

    Færdig

--- Opgave 3.4/Program.cs
+++ Opgave 3.4/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+Console.WriteLine("Hvor mange sider indholder dokomentet?");
+int sideantal = Convert.ToInt32(Console.ReadLine());
+
+int besparelse = sideantal / 2;
+
+Console.WriteLine($"Du spare {besparelse} sider ved at printe på begge sider.");
